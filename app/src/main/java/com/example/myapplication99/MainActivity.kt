9b635ab8ea918955034c1f48de1def8b61cf59e7
krasnovss91 <krasnovstanislav91@gmail.com>
2022-02-05 11:47:54 +0400

    Initial commit

---
+++ app/src/main/java/com/example/myapplication99/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.myapplication99
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("WrongViewCast")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button1 = findViewById<AppCompatButton>(R.id.button1)
+        val button2 = findViewById<AppCompatButton>(R.id.button2)
+
+        button1.setOnClickListener(object : View.OnClickListener{
+            override fun onClick(p0: View?) {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container,Fragment1())
+                    .commitAllowingStateLoss()
+            }
+
+        })
+
+        button2.setOnClickListener(object : View.OnClickListener{
+            override fun onClick(p0: View?) {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container,Fragment2())
+                    .commitAllowingStateLoss()
+            }
+        })
+    }
+}
