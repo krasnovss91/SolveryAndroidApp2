8f4b7f1671fc257e0e6463b52a3c050aecf4789a
krasnovss91 <krasnovstanislav91@gmail.com>
2022-02-05 11:48:27 +0400

    fragments_1

--- app/src/main/java/com/example/myapplication99/MainActivity.kt
+++ app/src/main/java/com/example/myapplication99/MainActivity.kt
@@ -15,19 +15,19 @@ class MainActivity : AppCompatActivity() {
         val button1 = findViewById<AppCompatButton>(R.id.button1)
         val button2 = findViewById<AppCompatButton>(R.id.button2)
 
-        button1.setOnClickListener(object : View.OnClickListener{
+        button1.setOnClickListener(object : View.OnClickListener {
             override fun onClick(p0: View?) {
                 supportFragmentManager.beginTransaction()
-                    .replace(R.id.fragment_container,Fragment1())
+                    .replace(R.id.fragment_container, Fragment1())
                     .commitAllowingStateLoss()
             }
 
         })
 
-        button2.setOnClickListener(object : View.OnClickListener{
+        button2.setOnClickListener(object : View.OnClickListener {
             override fun onClick(p0: View?) {
                 supportFragmentManager.beginTransaction()
-                    .replace(R.id.fragment_container,Fragment2())
+                    .replace(R.id.fragment_container, Fragment2())
                     .commitAllowingStateLoss()
             }
         })
